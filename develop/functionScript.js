9599ad5e3bc0252379c5d92815b2273a4d19827b
Joshua Jainga <jjainga@gmail.com>
2020-09-15 19:09:28 -0700

    Working Scoring Function

--- develop/functionScript.js
+++ develop/functionScript.js
@@ -3,6 +3,9 @@ var userAnswerA = document.querySelector("#AnswerA");
 var userAnswerB = document.querySelector("#AnswerB");
 var userAnswerC = document.querySelector("#AnswerC");
 var userAnswerD = document.querySelector("#AnswerD");
+// var myAnswer = document.querySelector(".answerBtn")
+var score = document.querySelector("#topScore").textContent;
+var totalScore = 0;
 
 //TODO:timer, onclick will start to count down
 
@@ -17,66 +20,83 @@ function countDown () {
             timer.textContent = "Complete!!";
             clearInterval(timeInterval);
         }}, 1000)
+
+        start.style.display = "none";
 }
 
 //TODO:Create function that will let the use click each answer and move to the next question
 
-function changeQuestion(event) {
-    //Creating what the question change to 
+function changeQuestion(event) {    
+    //Randomizing the question after each click
     var selectedQuestion = question[Math.floor(Math.random()*question.length)];
     var currentQuestion = selectedQuestion.Question;
-    
     //Randomizing answers from each quesiton
     var answerArr = [];
     answerArr.push(selectedQuestion.AnswerA);
     answerArr.push(selectedQuestion.AnswerB);
     answerArr.push(selectedQuestion.AnswerC);
     answerArr.push(selectedQuestion.AnswerD);
-
     //checking if the answerArr is built
-    console.log(answerArr)
-    var randomNumber = Math.floor(Math.random()*answerArr.length)
+    console.log(answerArr);
+    //Set index for correct question
+    correctAnswer = answerArr[0];
+    console.log(correctAnswer);
+    // user answer
+    var userAnswer = event.target.textContent;
+    console.log(userAnswer);
+    //Create scoring variable
+    if(userAnswer == correctAnswer) {
+        totalScore = totalScore + 10;
+        score = "Score: " + totalScore;
+        document.querySelector("#topScore").textContent = score;
+    }
+    else {
+        totalScore = totalScore - 10;
+        score = "Score: " + totalScore;
+        document.querySelector("#topScore").textContent = score;
+    }
+    //Setting an answer for A, B, C, D after each click
+    var randomNumber = Math.floor(Math.random()*answerArr.length);
     var selectedAnswerA = answerArr[randomNumber];
     document.querySelector("#AnswerA").textContent = selectedAnswerA;
-    answerArr.splice(randomNumber,1),
-    //Checking if splice works
+    answerArr.splice(randomNumber,1);
     console.log(answerArr);
-    var randomNumber2 = Math.floor(Math.random()*answerArr.length)
+    console.log(correctAnswer);
+
+    var randomNumber2 = Math.floor(Math.random()*answerArr.length);
     var selectedAnswerB = answerArr[randomNumber2];
     document.querySelector("#AnswerB").textContent = selectedAnswerB;
     answerArr.splice(randomNumber2,1);
     console.log(answerArr);
-    var randomNumber3 = Math.floor(Math.random()*answerArr.length)
+    console.log(correctAnswer);
+
+    var randomNumber3 = Math.floor(Math.random()*answerArr.length);
     var selectedAnswerC = answerArr[randomNumber3];
     document.querySelector("#AnswerC").textContent = selectedAnswerC;
     answerArr.splice(randomNumber3,1);
     console.log(answerArr);
-    var randomNumber4 = Math.floor(Math.random()*answerArr.length)
+    console.log(correctAnswer);
+
+    var randomNumber4 = Math.floor(Math.random()*answerArr.length);
     var selectedAnswerD = answerArr[randomNumber4];
     document.querySelector("#AnswerD").textContent = selectedAnswerD;
-    answerArr.splice(randomNumber4,1)
+    answerArr.splice(randomNumber4,1);
     console.log(answerArr);
+    console.log(correctAnswer);
 
+    // Changing the question after each click
     document.querySelector("#currentQuestion").textContent = currentQuestion;
-
+    
+    //Checking if values are being assigned after click
+    console.log("-----------------------")
+    console.log(score);
+    console.log(totalScore);
+ 
 }
 //Create Eventlisteners for each function the user is inteaded to call 
-start.addEventListener("click", function() {
-    countDown();
-})
-userAnswerA.addEventListener("click", function() {
-    changeQuestion();
-})
-userAnswerB.addEventListener("click", function() {
-    changeQuestion();
-})
-userAnswerC.addEventListener("click", function() {
-    changeQuestion();
-})
-userAnswerD.addEventListener("click", function() {
-    changeQuestion();
-})
-
-//TODO: Create a random generator that pulls each question from a question bank array
-
-//TODO: Create a random generator that can replace each question and answers with a new question/answer from a question bank
+start.addEventListener("click",countDown);
+// myAnswer.addEventListener("click", changeQuestion)
+userAnswerA.addEventListener("mousedown", changeQuestion);
+userAnswerB.addEventListener("mousedown", changeQuestion);
+userAnswerC.addEventListener("mousedown", changeQuestion);
+userAnswerD.addEventListener("mousedown", changeQuestion);
--- develop/objectScript.js
+++ develop/objectScript.js
@@ -16,6 +16,306 @@ for (var i=0; i<50; i++) {
 console.log(question);
 console.log("-----------------")
 //TODO:Create unique questions and answers
-question[0].Question = "Do you like Peanut butter?";
+question[0].Question = "2 + 2 = x";
+question[0].AnswerA = "4";
+question[0].AnswerB = "8";
+question[0].AnswerC = "22";
+question[0].AnswerD = "0";
+//
+question[0].Question = "24 + 32 = x";
+question[0].AnswerA = "56";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "6 + 58 = x";
+question[0].AnswerA = "64";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "136 + 398 = x";
+question[0].AnswerA = "434";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "4 + 3 + 8 * 3 = x";
+question[0].AnswerA = "31";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "5 * 5  = x";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+question[0].Question = "2 + 2 = ?";
+question[0].AnswerA = "";
+question[0].AnswerB = "";
+question[0].AnswerC = "";
+question[0].AnswerD = "";
+//
+
 
 console.log(question)
